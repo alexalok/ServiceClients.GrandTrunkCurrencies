922ee9be8d1a0d9c7e32308a46b99c88984c64a2
Alex Ch <aleks00799@gmail.com>
2024-02-27 17:17:38 +0300

    fix crash on http request error

--- IsKernel.ServiceClients.GrandTrunk.HistoricCurrencyConverter/Clients/Concrete/GrandTrunkCurrencyConversionService.cs
+++ IsKernel.ServiceClients.GrandTrunk.HistoricCurrencyConverter/Clients/Concrete/GrandTrunkCurrencyConversionService.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
-using RestSharp;
 using IsKernel.ServiceClients.GrandTrunk.HistoricCurrencyConverter.Clients.Abstract;
 using IsKernel.ServiceClients.GrandTrunk.HistoricCurrencyConverter.Contracts;
 using IsKernel.ServiceClients.GrandTrunk.HistoricCurrencyConverter.Exceptions;
-using System.Globalization;
+using RestSharp;
 
 namespace IsKernel.ServiceClients.GrandTrunk.HistoricCurrencyConverter.Clients.Concrete
 {
@@ -78,127 +78,134 @@ namespace IsKernel.ServiceClients.GrandTrunk.HistoricCurrencyConverter.Clients.C
             });
 
             return taskCompletionSource.Task;
-		}
-	
-		public Task<ConversionRate> GetConversionRateAsync(string fromCode, string toCode)
-		{
-			if(string.IsNullOrWhiteSpace(fromCode) == true)
-			{
-				throw new HistoricCurrencyConverterException("fromCode cannot be null or whitespace");
-			}
-			if(string.IsNullOrWhiteSpace(toCode) == true)
-			{
-				throw new HistoricCurrencyConverterException("toCode cannot be null or whitespace");
-			}
-			var task = GetConversionRateAsync(fromCode, toCode, null);
-			return task;
-		}
-		
-		public Task<ConversionRate> GetConversionRateAsync(string fromCode, string toCode, DateTime? date)
-		{
-			if(string.IsNullOrWhiteSpace(fromCode) == true)
-			{
-				throw new HistoricCurrencyConverterException("fromCode cannot be null or whitespace");
-			}
-			if(string.IsNullOrWhiteSpace(toCode) == true)
-			{
-				throw new HistoricCurrencyConverterException("toCode cannot be null or whitespace");
-			}
-			var taskCompletionSource = new TaskCompletionSource<ConversionRate>();
-			var request = new RestRequest();
-			if(date!=null)
-			{
-				if(date.Value.CompareTo(DateTime.Now) <= 0 )
-				{
-					request.Resource = CONVERT_OPTION 
-									   + OPTION_SEPARATOR + GetDateFormat(date)
-									   + OPTION_SEPARATOR + fromCode 
-									   + OPTION_SEPARATOR + toCode;
-				}
-				else
-				{
-					throw new HistoricCurrencyConverterException(ERROR_DATE_IN_THE_FUTURE);
-				}
-			}
-			else
-			{
-				request.Resource = CONVERT_LATEST_OPTION 
-								   + OPTION_SEPARATOR + fromCode
-								   + OPTION_SEPARATOR + toCode;
-			}
-			_client.ExecuteAsync(request).ContinueWith(t =>
-				{
-					try
-					{
-						var result = Decimal.Parse(t.Result.Content, NumberStyles.Float);
-						var conversionRate = new ConversionRate(fromCode, toCode, result, date);
-						taskCompletionSource.SetResult(conversionRate);
-					}
-					catch(Exception ex)
-					{
-						var conversionRate = new ConversionRate(fromCode, toCode, 0.0m, date);
-						taskCompletionSource.SetException(ex);
-					}
-				});
-			return taskCompletionSource.Task;
-		}
-				
-		public Task<List<ConversionRate>> GetConversionRateAsync(string fromCode, string toCode, 
-		                                              			 DateTime start, DateTime end)
-		{
-			if(string.IsNullOrWhiteSpace(fromCode) == true)
-			{
-				throw new HistoricCurrencyConverterException("fromCode cannot be null or whitespace");
-			}
-			if(string.IsNullOrWhiteSpace(toCode) == true)
-			{
-				throw new HistoricCurrencyConverterException("toCode cannot be null or whitespace");
-			}
-			if( (start.CompareTo(DateTime.Now) > 0 ) || (end.CompareTo(DateTime.Now) > 0) )
-			{
-				throw new HistoricCurrencyConverterException("start and end date must not be in the future");
-			}
-			if( start.CompareTo(end) > 0)
-			{
-				throw new HistoricCurrencyConverterException("The start date must not be later than the end date");
-			}			
-			const int DATE_INDEX = 0;
-			const int RATE_INDEX = 1;
-			var taskCompletionSource = new TaskCompletionSource<List<ConversionRate>>();
-			var conversionRates = new List<ConversionRate>();
-			var request = new RestRequest();
-			request.Resource = CONVERT_RANGE 
-							   + OPTION_SEPARATOR + GetDateFormat(start)
-							   + OPTION_SEPARATOR + GetDateFormat(end)
-							   + OPTION_SEPARATOR + fromCode 
-							   + OPTION_SEPARATOR + toCode;
-			_client.ExecuteAsync(request).ContinueWith(t =>
-				{
-					var separator = Environment.NewLine.ToCharArray();
-					var dateAndRateList = t.Result.Content.Split(separator).ToList();
-					//Eliminates the last element which is only whitespace
-					dateAndRateList.Remove(dateAndRateList.Last());
-					var conversionRatesList = new List<ConversionRate>();
-					foreach (var element in dateAndRateList) 
-					{
-						var rangeSeparator = RANGE_SEPARATOR.ToCharArray();
-						var dateAndRateArray = element.Split(rangeSeparator);		
-						try 
-						{
-							DateTime? date = DateTime.Parse(dateAndRateArray[DATE_INDEX]);
-							decimal? rate = Decimal.Parse(dateAndRateArray[RATE_INDEX]);
-							var conversionRate = new ConversionRate(fromCode, toCode, rate, date);	
-							conversionRatesList.Add(conversionRate);	
-						} 
-						catch(Exception exception) 
-						{
-							taskCompletionSource.SetException(exception);
-						}						
-					}	
-					taskCompletionSource.SetResult(conversionRatesList);							
-				});					
-			return taskCompletionSource.Task;
-		}
-		
-	}
+        }
+
+        public Task<ConversionRate> GetConversionRateAsync(string fromCode, string toCode)
+        {
+            if (string.IsNullOrWhiteSpace(fromCode) == true)
+            {
+                throw new HistoricCurrencyConverterException("fromCode cannot be null or whitespace");
+            }
+            if (string.IsNullOrWhiteSpace(toCode) == true)
+            {
+                throw new HistoricCurrencyConverterException("toCode cannot be null or whitespace");
+            }
+            var task = GetConversionRateAsync(fromCode, toCode, null);
+            return task;
+        }
+
+        public Task<ConversionRate> GetConversionRateAsync(string fromCode, string toCode, DateTime? date)
+        {
+            if (string.IsNullOrWhiteSpace(fromCode) == true)
+            {
+                throw new HistoricCurrencyConverterException("fromCode cannot be null or whitespace");
+            }
+            if (string.IsNullOrWhiteSpace(toCode) == true)
+            {
+                throw new HistoricCurrencyConverterException("toCode cannot be null or whitespace");
+            }
+            var taskCompletionSource = new TaskCompletionSource<ConversionRate>();
+            var request = new RestRequest();
+            if (date != null)
+            {
+                if (date.Value.CompareTo(DateTime.Now) <= 0)
+                {
+                    request.Resource = CONVERT_OPTION
+                                       + OPTION_SEPARATOR + GetDateFormat(date)
+                                       + OPTION_SEPARATOR + fromCode
+                                       + OPTION_SEPARATOR + toCode;
+                }
+                else
+                {
+                    throw new HistoricCurrencyConverterException(ERROR_DATE_IN_THE_FUTURE);
+                }
+            }
+            else
+            {
+                request.Resource = CONVERT_LATEST_OPTION
+                                   + OPTION_SEPARATOR + fromCode
+                                   + OPTION_SEPARATOR + toCode;
+            }
+            _client.ExecuteAsync(request).ContinueWith(t =>
+                {
+                    if (t.Result.ErrorException is { } errorEx)
+                    {
+                        var conversionRate = new ConversionRate(fromCode, toCode, 0.0m, date);
+                        taskCompletionSource.SetResult(conversionRate);
+                        return;
+                    }
+
+                    try
+                    {
+                        var result = Decimal.Parse(t.Result.Content, NumberStyles.Float);
+                        var conversionRate = new ConversionRate(fromCode, toCode, result, date);
+                        taskCompletionSource.SetResult(conversionRate);
+                    }
+                    catch (Exception ex)
+                    {
+                        var conversionRate = new ConversionRate(fromCode, toCode, 0.0m, date);
+                        taskCompletionSource.SetException(ex);
+                    }
+                });
+            return taskCompletionSource.Task;
+        }
+
+        public Task<List<ConversionRate>> GetConversionRateAsync(string fromCode, string toCode,
+                                                                   DateTime start, DateTime end)
+        {
+            if (string.IsNullOrWhiteSpace(fromCode) == true)
+            {
+                throw new HistoricCurrencyConverterException("fromCode cannot be null or whitespace");
+            }
+            if (string.IsNullOrWhiteSpace(toCode) == true)
+            {
+                throw new HistoricCurrencyConverterException("toCode cannot be null or whitespace");
+            }
+            if ((start.CompareTo(DateTime.Now) > 0) || (end.CompareTo(DateTime.Now) > 0))
+            {
+                throw new HistoricCurrencyConverterException("start and end date must not be in the future");
+            }
+            if (start.CompareTo(end) > 0)
+            {
+                throw new HistoricCurrencyConverterException("The start date must not be later than the end date");
+            }
+            const int DATE_INDEX = 0;
+            const int RATE_INDEX = 1;
+            var taskCompletionSource = new TaskCompletionSource<List<ConversionRate>>();
+            var conversionRates = new List<ConversionRate>();
+            var request = new RestRequest();
+            request.Resource = CONVERT_RANGE
+                               + OPTION_SEPARATOR + GetDateFormat(start)
+                               + OPTION_SEPARATOR + GetDateFormat(end)
+                               + OPTION_SEPARATOR + fromCode
+                               + OPTION_SEPARATOR + toCode;
+            _client.ExecuteAsync(request).ContinueWith(t =>
+                {
+                    var separator = Environment.NewLine.ToCharArray();
+                    var dateAndRateList = t.Result.Content.Split(separator).ToList();
+                    //Eliminates the last element which is only whitespace
+                    dateAndRateList.Remove(dateAndRateList.Last());
+                    var conversionRatesList = new List<ConversionRate>();
+                    foreach (var element in dateAndRateList)
+                    {
+                        var rangeSeparator = RANGE_SEPARATOR.ToCharArray();
+                        var dateAndRateArray = element.Split(rangeSeparator);
+                        try
+                        {
+                            DateTime? date = DateTime.Parse(dateAndRateArray[DATE_INDEX]);
+                            decimal? rate = Decimal.Parse(dateAndRateArray[RATE_INDEX]);
+                            var conversionRate = new ConversionRate(fromCode, toCode, rate, date);
+                            conversionRatesList.Add(conversionRate);
+                        }
+                        catch (Exception exception)
+                        {
+                            taskCompletionSource.SetException(exception);
+                        }
+                    }
+                    taskCompletionSource.SetResult(conversionRatesList);
+                });
+            return taskCompletionSource.Task;
+        }
+
+    }
 }
